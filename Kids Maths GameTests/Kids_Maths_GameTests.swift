a866980be740aa60658c797dd61bc63d8fae983b
jc14ach <jc14ach@cta-ab270-30.herts.ac.uk>
2017-02-21 14:14:34 +0000

    Initial Commit

---
+++ Kids Maths GameTests/Kids_Maths_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kids_Maths_GameTests.swift
+//  Kids Maths GameTests
+//
+//  Created by jc14ach on 21/02/2017.
+//  Copyright © 2017 jc14ach. All rights reserved.
+//
+
+import XCTest
+@testable import Kids_Maths_Game
+
+class Kids_Maths_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
